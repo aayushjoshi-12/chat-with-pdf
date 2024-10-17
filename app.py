765692b64d547351f6ed7a4c893b45bd430980c5
Aayush <joshiaayush1244@gmail.com>
2024-10-17 09:37:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_huggingface import HuggingFaceEmbeddings, HuggingFaceEndpoint
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+load_dotenv()
+
+access_token = os.environ.get("ACCESS_TOKEN")
+
+st.title("Chat with your PDF")
+
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+
+if uploaded_file is not None:
+    with open("temp.pdf", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    file_path = "temp.pdf"
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+
+    vectorstore = Chroma.from_documents(
+        documents=splits, embedding=HuggingFaceEmbeddings()
+    )
+    retriever = vectorstore.as_retriever()
+
+    system_prompt = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise. "
+        "\n\n"
+        "{context}"
+    )
+
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
+    )
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    llm = HuggingFaceEndpoint(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.3",
+        task="question-answering",
+        huggingfacehub_api_token=access_token,
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    chat_history = []
+
+    st.write("Ask your PDF a question:")
+
+    question = st.text_input("Your question:")
+    if question:
+        ai_msg = rag_chain.invoke({"input": question, "chat_history": chat_history})
+        st.markdown(ai_msg["answer"])
+        chat_history.extend(
+            [
+                HumanMessage(content=question),
+                AIMessage(content=ai_msg["answer"]),
+            ]
+        )
